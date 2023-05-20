31b33c495458b902d8d09b309b87e0625ba85da9
elijebrown <elijah.bruhn@gmail.com>
2023-05-20 13:52:12 -0400

    first front-end push

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
-}
+// Import the path module
+const path = require('path');
 
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
+// Node Server Setup 
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const indexRouter = require('./routes/index')
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout','layouts/layout')
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
 
-app.use(expressLayouts)
-app.use(express.static('public'))
+app.use('/static', express.static(path.join(__dirname, 'TickList')));
+app.use('/static', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
-// MongoDB setup
-const mongoose = require('mongoose')
-mongoose.connect(
-    process.env.DATABASE_URL, 
-    {useNewUrlParser: true}
-)
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.error('Connected to Mongoose'))
+// Serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
 
-app.use('/', indexRouter)
 
-app.listen(process.env.PORT || 3000)
+
+
+// // MongoDB setup
+// const mongoose = require('mongoose')
+// mongoose.connect(
+//     'mongodb://localhost:27017/admin',
+//     {useNewUrlParser: true, useUnifiedTopology: true}
+// )
+// const db = mongoose.connection
+// db.on('error', error => console.error(error))
+// db.once('open', () => console.error('Connected to Mongoose'))
